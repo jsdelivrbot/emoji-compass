8376421ef2f7f699d7b998b0c47ae0774c2596d8
Glitch (bristle-gateway) <none>
2018-03-09 15:18:17 +0000

    🏥🎒 Updated with Glitch

---
+++ chatdatacontrol/slack.js
@@ -0,0 +1,60 @@
+const {
+  fetchSlackHistory,
+  stringMessageText
+} = require("../chatdatamodel/slack.js");
+const {
+  fetchToneData,
+  fetchToneEmojis,
+  fetchToneScores
+} = require("../chatdatamodel/tone.js");
+
+function getSlackHistory(req, res, next) {
+  fetchSlackHistory()
+    .then(history => {
+      res.send(history);
+    })
+    .catch(next);
+}
+
+function getSlackToneData(req, res, next) {
+  fetchSlackHistory()
+    .then(history => {
+      stringMessageText(history, (err, text) => {
+        fetchToneData(text, (err, data) => {
+          res.send(data);
+        });
+      });
+    })
+    .catch(next);
+}
+
+function getSlackEmojis(cb) {
+  fetchSlackHistory()
+    .then(history => {
+      stringMessageText(history, (err, text) => {
+        fetchToneEmojis(text, (err, emojis) => {
+          cb(null, emojis);
+        });
+      });
+    })
+    .catch(next);
+}
+
+function getSlackScores(cb) {
+  fetchSlackHistory()
+    .then(history => {
+      stringMessageText(history, (err, text) => {
+        fetchToneScores(text, (err, scores) => {
+          cb(null, scores);
+        });
+      });
+    })
+    .catch(next);
+}
+
+module.exports = {
+  getSlackHistory,
+  getSlackToneData,
+  getSlackEmojis,
+  getSlackScores
+};
---
+++ skills/getChatMood.js
@@ -0,0 +1,22 @@
+const {
+  getSlackEmojis,
+  getSlackHistory
+} = require("../chatdatacontrol/slack.js");
+const { fetchSlackHistory, stringMessageText } = require("../chatdatamodel/slack.js");
+
+
+
+module.exports = function(controller) {
+  controller.hears(
+    ["mood"],
+    "direct_message,direct_mention",
+    (bot, message) => {
+      bot.startConversation(message, function(err, convo) {
+        fetchSlackHistory().then(history => {
+          convo.say(JSON.stringify(history));
+        convo.next();
+        }).catch(err);
+      })
+    }
+  );
+};
